0fdbb1e76d90b49527a8954d1bd2bbaace4ef53e
Alvin <alvinbungur743@gmail.com>
2025-10-11 02:16:44 +0700

    Fix some minor problems

--- database/migrations/2025_10_10_191613_create_category_product_table.php
+++ database/migrations/2025_10_10_191613_create_category_product_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            //
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_category_name', 191)->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('category_product');
     }
 };
