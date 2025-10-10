1eab8c5a672bb43a1fad043b0a71d4e57694b1ed
Alvin <alvinbungur743@gmail.com>
2025-10-11 03:15:05 +0700

    Lupa save

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ProductSeeder::class,
             ProductCategorySeeder::class,
             SupplierSeeder::class,
         ]);
