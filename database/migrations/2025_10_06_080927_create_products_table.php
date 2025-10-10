f806834892f63c60950ba42dcb13a914b7995173
Alvin <alvinbungur743@gmail.com>
2025-10-11 03:07:56 +0700

    Minor problem fix

--- database/migrations/2025_10_06_080927_create_products_table.php
+++ database/migrations/2025_10_06_080927_create_products_table.php
@@ -22,19 +22,6 @@ return new class extends Migration
             $table->integer('stock')->default(0);
             $table->timestamps();
         });
-
-        Schema::create('category_product', function (Blueprint $table) {
-            $table->id();
-            $table->string('product_category_name');
-            $table->timestamps();
-        });
-
-        Schema::create('supplier', function (Blueprint $table) {
-            $table->id();
-            $table->string('supplier_name');
-            $table->string('pic_supplier');
-            $table->timestamps();
-        });
     }
 
     /**
